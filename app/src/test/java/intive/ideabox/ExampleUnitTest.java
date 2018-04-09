8c9bb2ac02d0589d0df48f670353081c8bf4603c
Artur <R_E_D_O_X@wp.pl>
2018-04-09 22:04:43 +0200

    Added mockito interface test

--- app/src/test/java/intive/ideabox/ExampleUnitTest.java
+++ app/src/test/java/intive/ideabox/ExampleUnitTest.java
@@ -1,17 +1,59 @@
 package intive.ideabox;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.util.Arrays;
+import java.util.List;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
+import intive.ideabox.models.DataProvider;
+import intive.ideabox.models.IdeaData;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
 public class ExampleUnitTest {
-    @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+
+    private static DataProvider mockedDataProvider;
+    private static IdeaData ideaData1;
+    private static IdeaData ideaData2;
+
+
+    @BeforeClass
+    public static void setUp(){
+        mockedDataProvider = mock(DataProvider.class);
+
+        ideaData1 = new IdeaData("pierwsza idea", "pierwszy user");
+        ideaData2 = new IdeaData("druga idea", "drugi user");
+
+        when(mockedDataProvider.saveIdea("pierwsza idea")).thenReturn(true);
+        when(mockedDataProvider.loadIdea()).thenReturn(Arrays.asList(ideaData1, ideaData2));
+
     }
+
+    @Test
+    public void testInterfaceMethods() throws Exception {
+
+        List<IdeaData> testLoadIdeaData1 = mockedDataProvider.loadIdea();
+        Boolean testSaveIdeaData2 = mockedDataProvider.saveIdea("pierwsza idea");
+
+        assertNotNull(testSaveIdeaData2);
+        assertNotNull(testLoadIdeaData1);
+
+        IdeaData testIdeaData = testLoadIdeaData1.get(0);
+
+        assertEquals(true, testSaveIdeaData2);
+        assertEquals("pierwsza idea", testIdeaData.ideatext);
+        assertEquals("pierwszy user", testIdeaData.ideauser);
+
+        testIdeaData = testLoadIdeaData1.get(1);
+        assertEquals("druga idea", testIdeaData.ideatext);
+        assertEquals("drugi user", testIdeaData.ideauser);
+
+
+    }
+
+
 }
