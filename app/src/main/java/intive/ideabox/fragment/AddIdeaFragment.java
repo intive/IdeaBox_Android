5e98c9aa3334a4334107d5a08f7ec8d71d5a9180
lepetinez <lepetinez@gmail.com>
2018-04-14 01:43:37 +0200

    hiding keyboard issue fixed

--- app/src/main/java/intive/ideabox/fragment/AddIdeaFragment.java
+++ app/src/main/java/intive/ideabox/fragment/AddIdeaFragment.java
@@ -1,5 +1,6 @@
 package intive.ideabox.fragment;
 
+import android.content.Context;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -7,24 +8,34 @@ import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 
 import intive.ideabox.R;
 import intive.ideabox.databinding.FragmentAddIdeaBinding;
-import intive.ideabox.model.IdeaData;
 import intive.ideabox.viewmodel.AddIdeaViewModel;
 
 public class AddIdeaFragment extends android.support.v4.app.Fragment {
 
     public FragmentActivity mainContext;
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        final InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(getView().getWindowToken(), 0);
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable final Bundle savedInstanceState) {
         FragmentAddIdeaBinding binding= DataBindingUtil.inflate(inflater, R.layout.fragment_add_idea, container, false);
-        IdeaData ideaData = new IdeaData("default idea text", "defUser");
+        String idea = "";
         AddIdeaViewModel viewModel = new AddIdeaViewModel(getActivity());
         binding.setIdeaViewModel(viewModel);
-        binding.setUserIdea(ideaData);
+        binding.setIdea(idea);
+        viewModel.setupParent(binding.getRoot());
         return binding.getRoot();
+
     }
+
 }
