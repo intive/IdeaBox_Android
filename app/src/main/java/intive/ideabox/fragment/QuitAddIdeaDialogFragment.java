fdd02702b705e9214d0da18d89f5beeea3d73e39
lepetinez <lepetinez@gmail.com>
2018-04-17 19:55:56 +0200

    dismissing dialog functionality added

--- app/src/main/java/intive/ideabox/fragment/QuitAddIdeaDialogFragment.java
+++ app/src/main/java/intive/ideabox/fragment/QuitAddIdeaDialogFragment.java
@@ -16,10 +16,12 @@ public class QuitAddIdeaDialogFragment  extends DialogFragment{
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         FragmentQuitAddIdeaBinding binding = DataBindingUtil.inflate(LayoutInflater.from(getActivity()), R.layout.fragment_quit_add_idea, null, false);
-        QuitAddIdeaViewModel quitAddIdeaViewModel = new QuitAddIdeaViewModel(((MainActivity)getActivity()));
-        binding.setDialogViewModel(quitAddIdeaViewModel);
-        return new AlertDialog.Builder(getActivity(), R.style.ThemeOverlay_AppCompat_Dialog)
-                .setView(binding.getRoot()).setMessage("Warning")
+        Dialog dialog  = new AlertDialog.Builder(getActivity())
+                .setView(binding.getRoot())
                 .create();
+        QuitAddIdeaViewModel quitAddIdeaViewModel = new QuitAddIdeaViewModel(((MainActivity)getActivity()),dialog);
+        binding.setDialogViewModel(quitAddIdeaViewModel);
+        return dialog;
+
     }
 }
