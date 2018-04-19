8feab05bf79ff502a0cd888848287e01d67a943f
Artur <R_E_D_O_X@wp.pl>
2018-04-19 23:07:36 +0200

    Fixed bug - Landscape view IBPPFIP-54

--- app/src/main/java/intive/ideabox/activity/MainActivity.java
+++ app/src/main/java/intive/ideabox/activity/MainActivity.java
@@ -12,7 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if(null == savedInstanceState)
         getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, IdeaListFragment.newInstance(false)).commit();
     }
 
