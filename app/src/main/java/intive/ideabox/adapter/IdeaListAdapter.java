df21712ec62bb930e6134d553b961b6ff7e2e6d1
Bumbl3Bee <grzegorz.baczek.93@gmail.com>
2018-05-08 00:20:10 +0200

    Deleted unused code

--- app/src/main/java/intive/ideabox/adapter/IdeaListAdapter.java
+++ app/src/main/java/intive/ideabox/adapter/IdeaListAdapter.java
@@ -4,16 +4,13 @@ import java.util.Collections;
 import java.util.List;
 
 import intive.ideabox.model.IdeaData;
-import intive.ideabox.utility.RandomColorGenerator;
 
 public class IdeaListAdapter extends SingleLayoutAdapter{
 
     private List<IdeaData> ideaData = Collections.emptyList();
-    private RandomColorGenerator colorGenerator;
 
     public IdeaListAdapter(int layoutId) {
         super(layoutId);
-        colorGenerator = new RandomColorGenerator();
     }
 
     @Override
@@ -24,10 +21,6 @@ public class IdeaListAdapter extends SingleLayoutAdapter{
     public void setData(List<IdeaData> data){
         ideaData = data;
         notifyDataSetChanged();
-    }
-
-    public int getColor(){
-        return colorGenerator.getRandomColor();
     }
 
     @Override
