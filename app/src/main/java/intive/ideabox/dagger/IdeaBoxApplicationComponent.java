cdc81840224d0e8dc1420b9783fef144e2c90b6d
Bumbl3Bee <grzegorz.baczek.93@gmail.com>
2018-06-04 21:35:00 +0200

    Fixed issue

--- app/src/main/java/intive/ideabox/dagger/IdeaBoxApplicationComponent.java
+++ app/src/main/java/intive/ideabox/dagger/IdeaBoxApplicationComponent.java
@@ -4,10 +4,12 @@ import javax.inject.Singleton;
 
 import dagger.Component;
 import intive.ideabox.viewmodel.AuthenticationViewModel;
+import intive.ideabox.viewmodel.DetailIdeaViewModel;
 
 @Singleton
 @Component(modules = {IdeaBoxApplicationModule.class})
 public interface IdeaBoxApplicationComponent {
 
     void inject(AuthenticationViewModel authenticationViewModel);
+    void inject(DetailIdeaViewModel detailIdeaViewModel);
 }
