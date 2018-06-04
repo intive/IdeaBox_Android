48ddfb322907bc606545f5fac8d757545c52fbda
Asia Roslaniec <joanna.roslaniec@gmail.com>
2018-06-04 21:12:46 +0200

    >add new espresso test

--- app/src/androidTest/java/intive/ideabox/activity/ScreenAdmin.java
+++ app/src/androidTest/java/intive/ideabox/activity/ScreenAdmin.java
@@ -16,6 +16,8 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
@@ -41,7 +43,8 @@ public class ScreenAdmin {
         onView(withId(R.id.login_button))
                 .perform(click());
 
-
+        onView(withId(R.id.idea_recycler))
+            .check(matches(isDisplayed()));
 
     }
 }
